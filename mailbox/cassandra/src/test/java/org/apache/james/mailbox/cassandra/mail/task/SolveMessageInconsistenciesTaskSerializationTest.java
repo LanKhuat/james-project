b4327523acd9c9326d91cd90aa1516e9e7bd075e
LanKhuat <khuatdanglan@gmail.com>
2020-05-21 15:48:12 +0700

    fixup! JAMES-3184 Update task serialization/deserialization with RunningOptions

--- mailbox/cassandra/src/test/java/org/apache/james/mailbox/cassandra/mail/task/SolveMessageInconsistenciesTaskSerializationTest.java
+++ mailbox/cassandra/src/test/java/org/apache/james/mailbox/cassandra/mail/task/SolveMessageInconsistenciesTaskSerializationTest.java
@@ -71,7 +71,7 @@ public class SolveMessageInconsistenciesTaskSerializationTest {
     }
 
     @Test
-    void legacyTaskshouldBeDeserializable() throws Exception {
+    void legacyTaskShouldBeDeserializable() throws Exception {
         SolveMessageInconsistenciesService service = mock(SolveMessageInconsistenciesService.class);
 
         SolveMessageInconsistenciesTask legacyTask = JsonGenericSerializer.forModules(SolveMessageInconsistenciesTaskDTO.module(service))
